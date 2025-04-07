c94fa7cb04129c2aba5035cb6a6a0e8d5b7a14c5
Arda <arda1033@gmail.com>
2025-04-07 18:36:52 +0300

    dfg

--- main.py
+++ main.py
@@ -3,113 +3,108 @@ import jsonpickle
 import datetime
 
 
-a = pd.read_excel("aksam.xlsx").to_json()
-# convert to turkish encoding
-b = a.encode("utf-8").decode("latin-1")
-obj = jsonpickle.decode(b)
+def to_calendar(dosya: str) -> dict:
+    a = pd.read_excel(dosya).to_json()
+    # convert to turkish encoding
+    b = a.encode("utf-8").decode("latin-1")
+    obj = jsonpickle.decode(b)
 
+    obj2 = {}
+    for key in obj:
+        if key.startswith("Unnamed: "):
+            obj2[int(key.replace("Unnamed: ", ""))] = obj[key]
+        else:
+            obj2[key] = obj[key]
 
-obj2 = {}
-for key in obj:
-    if key.startswith("Unnamed: "):
-        obj2[int(key.replace("Unnamed: ", ""))] = obj[key]
-    else:
-        obj2[key] = obj[key]
+    obj = obj2
 
-
-obj = obj2
-
-
-clean_obj = {}
-for q in obj:
-    for z in obj[q]:
-        if clean_obj.get(q) is None:
-            clean_obj[q] = {}
-        if obj[q][z] == None:
-            continue
-        clean_obj[q][int(z)] = obj[q][z]
-
-
-days = {}
-for x in clean_obj.keys():
-    for y in clean_obj[x]:
-        if type(clean_obj[x][y]) == int:
-            days[
-                datetime.datetime.fromtimestamp(int(clean_obj[x][y]) / 1000).strftime(
-                    "%d/%m/%Y"
-                )
-            ] = []
-            y1 = y + 1
-            try:
-                while True:
-                    if type(clean_obj[x][y1]) == int:
-                        break
-                    days[
-                        datetime.datetime.fromtimestamp(
-                            int(clean_obj[x][y]) / 1000
-                        ).strftime("%d/%m/%Y")
-                    ].append(clean_obj[x][y1])
-                    y1 = y1 + 1
-            except KeyError:
+    clean_obj = {}
+    for q in obj:
+        for z in obj[q]:
+            if clean_obj.get(q) is None:
+                clean_obj[q] = {}
+            if obj[q][z] == None:
                 continue
+            clean_obj[q][int(z)] = obj[q][z]
 
+    days = {}
+    for x in clean_obj.keys():
+        for y in clean_obj[x]:
+            if type(clean_obj[x][y]) == int:
+                days[
+                    datetime.datetime.fromtimestamp(
+                        int(clean_obj[x][y]) / 1000
+                    ).strftime("%d/%m/%Y")
+                ] = []
+                y1 = y + 1
+                try:
+                    while True:
+                        if type(clean_obj[x][y1]) == int:
+                            break
+                        days[
+                            datetime.datetime.fromtimestamp(
+                                int(clean_obj[x][y]) / 1000
+                            ).strftime("%d/%m/%Y")
+                        ].append(clean_obj[x][y1])
+                        y1 = y1 + 1
+                except KeyError:
+                    continue
 
-clear_days = jsonpickle.decode(jsonpickle.encode(days).replace("*", ""))
+    clear_days = jsonpickle.decode(jsonpickle.encode(days).replace("*", ""))
 
-try:
-    clear_days.pop("01/01/1970")
-except KeyError:
-    pass  # remove the key if it exists
+    try:
+        clear_days.pop("01/01/1970")
+    except KeyError:
+        pass  # remove the key if it exists
 
-del clean_obj, obj, b, a, x, y, y1, key, obj2, days
+    del clean_obj, obj, b, a, x, y, y1, key, obj2, days
 
-counter = 0
-for y in range(1, 32):
-    if len(str(y)) == 1:
-        i = "0" + str(y)
-    else:
-        i = str(y)
+    counter = 0
+    for y in range(1, 32):
+        if len(str(y)) == 1:
+            i = "0" + str(y)
+        else:
+            i = str(y)
+        for key in clear_days.keys():
+            if key.startswith(f"{i}/"):
+                counter += 1
+                break
+
+    calendar = {}
     for key in clear_days.keys():
-        if key.startswith(f"{i}/"):
-            counter += 1
-            break
+        calendar[key[:5]] = clear_days[key]
 
+    days_in_a_month = {
+        "01": 31,
+        "02": 28,
+        "03": 31,
+        "04": 30,
+        "05": 31,
+        "06": 30,
+        "07": 31,
+        "08": 31,
+        "09": 30,
+        "10": 31,
+        "11": 30,
+        "12": 31,
+    }
 
-calendar = {}
-for key in clear_days.keys():
-    calendar[key[:5]] = clear_days[key]
-
-days_in_a_month = {
-    "01": 31,
-    "02": 28,
-    "03": 31,
-    "04": 30,
-    "05": 31,
-    "06": 30,
-    "07": 31,
-    "08": 31,
-    "09": 30,
-    "10": 31,
-    "11": 30,
-    "12": 31,
-}
-
-for key in calendar.keys():
-    a = days_in_a_month[key[3:]]
-    b = len(calendar.keys())
-
-    if a == b:
-        print("all days validated")
-    else:
-        print("not all days validated")
-
-    break
-
-if False:
     for key in calendar.keys():
-        print(key, calendar[key])
-        print("-------------")
+        a = days_in_a_month[key[3:]]
+        b = len(calendar.keys())
 
+        if a == b:
+            print("all days validated")
+        else:
+            print("not all days validated")
+
+        break
+
+    return calendar
+
+
+calendar_sabah = to_calendar("sabah.xlsx")
+calendar_aksam = to_calendar("aksam.xlsx")
 
 import uvicorn
 from fastapi import FastAPI
@@ -118,28 +113,52 @@ app = FastAPI()
 
 
 @app.get("/")
-def read_root():
-    return calendar
-
-
-@app.get("/today")
+@app.get("/bugun")
 def read_today():
     today = datetime.datetime.now().strftime("%d/%m")
-    return calendar[today]
+    return {"sabah": calendar_sabah[today], "aksam": calendar_aksam[today]}
 
 
-@app.get("/tomorrow")
+@app.get("/yarin")
 def read_tomorrow():
     tomorrow = (datetime.datetime.now() + datetime.timedelta(days=1)).strftime("%d/%m")
-    return calendar[tomorrow]
+    return {"sabah": calendar_sabah[tomorrow], "aksam": calendar_aksam[tomorrow]}
+
+
+@app.get("/bugun/sabah")
+def read_today_sabah():
+    today = datetime.datetime.now().strftime("%d/%m")
+    return {"sabah": calendar_sabah[today]}
+
+
+@app.get("/bugun/aksam")
+def read_today_aksam():
+    today = datetime.datetime.now().strftime("%d/%m")
+    return {"aksam": calendar_aksam[today]}
+
+
+@app.get("/yarin/sabah")
+def read_tomorrow_sabah():
+    tomorrow = (datetime.datetime.now() + datetime.timedelta(days=1)).strftime("%d/%m")
+    return {"sabah": calendar_sabah[tomorrow]}
+
+
+@app.get("/yarin/aksam")
+def read_tomorrow_aksam():
+    tomorrow = (datetime.datetime.now() + datetime.timedelta(days=1)).strftime("%d/%m")
+    return {"aksam": calendar_aksam[tomorrow]}
 
 
 @app.get("/day/{day}")
 def read_day(day: str):
     try:
-        return calendar[day]
+        return {
+            "sabah": calendar_sabah[day],
+            "aksam": calendar_aksam[day],
+        }
     except KeyError:
         return {"error": "Day not found"}
+
 
 if __name__ == "__main__":
     uvicorn.run(host="0.0.0.0", port=2000, app=app)
