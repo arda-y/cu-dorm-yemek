a27d37f8fa858ce78715f26ab4b155dfde961152
Arda <arda1033@gmail.com>
2025-06-01 21:31:05 +0300

    haziran

--- main.py
+++ main.py
@@ -126,7 +126,7 @@ def construct_response(
     if not accept_status:
         pretty_json = json.dumps(obj, ensure_ascii=False, indent=2)
 
-        links = "reklam alinir<br>"
+        links = "reklamsiz hava sahasi<br>"
         if path != "/yarin":
             links += '<a href="https://yemekhane.vercel.app/yarin">Yarının listesi için tıkla gülüm</a><br>'
         if path != "/bugun" and path != "/":
